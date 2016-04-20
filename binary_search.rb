a17d9f6cc9f8512d7c4ff938e4a59aac9b99b9d0
Kurt Schlueter <kurtbschlueter@gmail.com>
2016-04-20 12:35:03 -0500

    rspec tests finished

--- binary_search.rb
+++ binary_search.rb
@@ -14,12 +14,12 @@ def binary_search(symbol, array)
   end
 end
 
-symbols = [:A, :B, :C, :D, :E, :F, :G]
+# symbols = [:A, :B, :C, :D, :E, :F, :G]
 
-p "C is: " + binary_search(:C, symbols).to_s
-p "D is: " + binary_search(:D, symbols).to_s
-p "A is: " + binary_search(:A, symbols).to_s
-p "B is: " + binary_search(:B, symbols).to_s
-p "E is: " + binary_search(:E, symbols).to_s
-p "F is: " + binary_search(:F, symbols).to_s
-p "G is: " + binary_search(:G, symbols).to_s
+# p "C is: " + binary_search(:C, symbols).to_s
+# p "D is: " + binary_search(:D, symbols).to_s
+# p "A is: " + binary_search(:A, symbols).to_s
+# p "B is: " + binary_search(:B, symbols).to_s
+# p "E is: " + binary_search(:E, symbols).to_s
+# p "F is: " + binary_search(:F, symbols).to_s
+# p "G is: " + binary_search(:G, symbols).to_s
--- binary_search_spec.rb
+++ binary_search_spec.rb
@@ -3,7 +3,31 @@ require_relative('binary_search.rb')
 describe 'Finds index of ceratin symbols in an array' do
   let(:symbols) { $symbols = [:A, :B, :C, :D, :E, :F, :G] }
 
+  it "returns 0 if symbol is :A" do
+    expect(binary_search(:A, symbols)).to eq(0)
+  end
+
+  it "returns 1 if symbol is :B" do
+    expect(binary_search(:B, symbols)).to eq(1)
+  end
+
   it "returns 2 if symbol is :C" do
     expect(binary_search(:C, symbols)).to eq(2)
+  end
+
+  it "returns 3 if symbol is :D" do
+    expect(binary_search(:D, symbols)).to eq(3)
+  end
+
+  it "returns 4 if symbol is :E" do
+    expect(binary_search(:E, symbols)).to eq(4)
+  end
+
+  it "returns 5 if symbol is :F" do
+    expect(binary_search(:F, symbols)).to eq(5)
+  end
+
+  it "returns 6 if symbol is :G" do
+    expect(binary_search(:G, symbols)).to eq(6)
   end
 end
